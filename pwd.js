e54f84089fa98c6bee095c05b370b857449bd0f5
shapirodaniel <shapirodanieladam@gmail.com>
2021-01-15 13:32:13 -0600

    added comments

--- pwd.js
+++ pwd.js
@@ -1,12 +1,10 @@
 function PWD() {
+	// initial prompt
 	process.stdout.write('\nprompt > ');
 	return process.stdin.on('data', data => {
 		// this stores whatever we input
 		const cmd = data.toString().trim();
-
-		// if cmd === pwd, do something
 		// process.cwd returns current working dir of node.js process
-
 		if (cmd === 'pwd') {
 			process.stdout.write(process.cwd());
 		}
